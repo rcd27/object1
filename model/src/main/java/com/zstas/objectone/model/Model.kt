d962905268810324cf143567c450c0ab7cd0d77f
Zemlyakov Stas <redtom@yandex.ru>
2018-09-10 11:57:34 +0300

    Add default test case.

--- model/src/main/java/com/zstas/objectone/model/Model.kt
+++ model/src/main/java/com/zstas/objectone/model/Model.kt
@@ -2,6 +2,6 @@ package com.zstas.objectone.model
 
 class Model
 
-fun main(args: Array<String>) {
-    println("Ama kotlena! Amama KoTLena!")
+fun test(): String {
+    return "Test"
 }
---
+++ model/src/test/java/com/zstas/objectone/model/ModelTest.java
@@ -0,0 +1,12 @@
+package com.zstas.objectone.model;
+
+import org.junit.Test;
+
+public class ModelTest {
+
+  @Test
+  public void main() {
+    Model m = new Model();
+    System.out.println("Test");
+  }
+}
