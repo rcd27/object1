aced87d86184f4d94e7bd13ddf8620579e4d71f1
Zemlyakov Stas <redtom@yandex.ru>
2018-09-14 12:14:44 +0300

    Add unit-test coverage reports

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("kotlin-android")
     id("kotlin-android-extensions")
+    id("jacoco")
 }
 
 android {
@@ -15,7 +16,7 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
-        all {
+        getByName("release") {
             isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
@@ -33,4 +34,19 @@ dependencies {
     testImplementation("junit:junit:4.12")
     androidTestImplementation("androidx.test:runner:1.1.0-alpha4")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.1.0-alpha4")
+}
+
+tasks {
+    val unitTestsReport by registering(JacocoReport::class) {
+        dependsOn("testDebugUnitTest")
+        group = "reports"
+        description = "Generate Jacoco test-coverage report"
+        classDirectories = fileTree("${project.buildDir}/tmp/kotlin-classes/debugUnitTest/")
+        sourceDirectories = files("${project.projectDir}/src/main/java")
+        executionData = files("${project.buildDir}/jacoco/testDebugUnitTest.exec")
+        reports {
+            html.isEnabled = true
+            html.destination = file("${project.projectDir}/../coverage/app")
+        }
+    }
 }
