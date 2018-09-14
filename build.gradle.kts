74cc314a66d40aec165dbf2a11582b93f7c5b6de
Zemlyakov Stas <redtom@yandex.ru>
2018-09-14 09:29:27 +0300

    Migrate app.build.gradle to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+buildscript {
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.3.0-alpha10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.2.61")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+}
