edd298a134817146944a1103d1e109a3ad255acc
Zemlyakov Stas <redtom@yandex.ru>
2018-09-14 15:37:11 +0300

    Add simple ui test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.sun.jndi.toolkit.url.Uri
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -31,8 +33,11 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.0.0-rc02")
     implementation("androidx.core:core-ktx:1.0.0-rc02")
     implementation("androidx.legacy:legacy-support-v4:1.0.0-rc02")
+
     testImplementation("junit:junit:4.12")
     androidTestImplementation("androidx.test:runner:1.1.0-alpha4")
+    androidTestImplementation("androidx.test:rules:1.1.0-alpha4")
+    androidTestImplementation("androidx.test.uiautomator:uiautomator-v18:2.2.0-alpha1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.1.0-alpha4")
 }
 
