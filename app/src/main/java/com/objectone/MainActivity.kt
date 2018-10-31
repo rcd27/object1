44e09df53e889c0232ca408b8663b76ae667ae8c
Zemlyakov Stas <redtom@yandex.ru>
2018-10-31 14:13:06 +0300

    Convert Java classes to Kotlin

---
+++ app/src/main/java/com/objectone/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.objectone
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.objectone.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/objectone/ui/main/MainViewModel.kt
@@ -0,0 +1,5 @@
+package com.objectone.ui.main
+
+import android.arch.lifecycle.ViewModel
+
+class MainViewModel : ViewModel()// TODO: Implement the ViewModel
