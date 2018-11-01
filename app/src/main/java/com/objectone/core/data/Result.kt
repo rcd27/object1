848ca51132375c14840f2428d135c488b0ce2b9c
Zemlyakov Stas <redtom@yandex.ru>
2018-11-01 17:27:00 +0300

    Improve sealed class and add some TODO/FIXME

--- app/src/main/java/com/objectone/core/data/Result.kt
+++ app/src/main/java/com/objectone/core/data/Result.kt
@@ -3,8 +3,7 @@ package com.objectone.core.data
 /**
  * A generic class that holds a value with its
  */
-sealed class Result<out T : Any> {
+sealed class Result<out T : Any>
 
-    data class Success<out T : Any>(val data: T) : Result<T>()
-    data class Error(val exception: Exception) : Result<Nothing>()
-}
+data class Success<out T : Any>(val data: T) : Result<T>()
+data class Error(val exception: Exception) : Result<Nothing>()
--- app/src/main/java/com/objectone/ui/list/ListViewModel.kt
+++ app/src/main/java/com/objectone/ui/list/ListViewModel.kt
@@ -4,12 +4,14 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.objectone.core.data.Result
+import com.objectone.core.data.Error
+import com.objectone.core.data.Success
 import com.objectone.core.list.ListRepository
 import com.objectone.core.list.data.ObjectOneItem
 
 class ListViewModel(listRepository: ListRepository) : ViewModel() {
 
+    // FIXME: not sure this is necessary. Why not just pass `_showItems`?
     private val _showItems = MutableLiveData<List<ObjectOneItem>>()
     val showItems: LiveData<List<ObjectOneItem>>
         get() = _showItems
@@ -17,12 +19,13 @@ class ListViewModel(listRepository: ListRepository) : ViewModel() {
     init {
         val result = listRepository.getList()
         when (result) {
-            is Result.Success -> _showItems.value = result.data
-            is Result.Error -> throw IllegalStateException("Error loading list")
+            is Success -> _showItems.value = result.data
+            is Error -> throw IllegalStateException("Error loading list")
         }
     }
 }
 
+// TODO: move this ugly motherfucker to `providers` after we have some DI
 @Suppress("UNCHECKED_CAST")
 class ListViewModelFactory(private val listRepository: ListRepository) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
