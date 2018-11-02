d822adde8c8b61e65716adc67af8bd9587cee275
Zemlyakov Stas <redtom@yandex.ru>
2018-11-02 17:24:26 +0300

    Fix type

--- app/src/main/java/com/objectone/core/list/ListRepository.kt
+++ app/src/main/java/com/objectone/core/list/ListRepository.kt
@@ -1,10 +1,11 @@
 package com.objectone.core.list
 
 import com.objectone.core.data.Result
+import com.objectone.core.data.Success
 import com.objectone.core.list.data.ObjectOneItem
 
 class ListRepository {
     fun getList(): Result<List<ObjectOneItem>> {
-        return Result.Success(emptyList())
+        return Success(emptyList())
     }
 }
