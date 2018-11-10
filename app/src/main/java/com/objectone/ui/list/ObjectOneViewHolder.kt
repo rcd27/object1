28a19090ff36fded2cba06e1eddfc6225de87517
Zemlyakov Stas <redtom@yandex.ru>
2018-11-10 18:21:35 +0300

    Fix glide implementation

--- app/src/main/java/com/objectone/ui/list/ObjectOneViewHolder.kt
+++ app/src/main/java/com/objectone/ui/list/ObjectOneViewHolder.kt
@@ -1,17 +1,25 @@
 package com.objectone.ui.list
 
 import android.view.View
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestOptions
 import com.objectone.core.list.data.ObjectOneItem
+import kotlinx.android.synthetic.main.view_list_item.view.*
 
-class ObjectOneViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class ObjectOneViewHolder(private val fragment: Fragment, itemView: View) : RecyclerView.ViewHolder(itemView) {
     fun bind(viewObject: ObjectOneItem) {
         itemView.contentDescription = viewObject.id // for testing purpose
-        // TODO: data is put here
-        /*
         itemView.item_list_id.text = viewObject.id
-        itemView.item_list_avatar.drawable = viewObject.image
+        /* WARNING */
+        Glide.with(fragment)
+                .load(viewObject.image)
+                .apply(RequestOptions.centerCropTransform())
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .into(itemView.item_list_avatar)
+        /* END OF WARNING */
         itemView.item_list_title.text = viewObject.title
-        */
     }
 }
